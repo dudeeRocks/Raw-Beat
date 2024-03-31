4aec0c682c6c0aa41d673c392b6cf9268f7adcf1
David Katsman <d.katsman@icloud.com>
2024-03-31 14:31:18 +0300

    Initial Commit

---
+++ Raw Beat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Raw Beat
+//
+//  Created by David Katsman on 31/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Raw Beat/Raw_BeatApp.swift
@@ -0,0 +1,17 @@
+//
+//  Raw_BeatApp.swift
+//  Raw Beat
+//
+//  Created by David Katsman on 31/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct Raw_BeatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
